93e6382dfac6c2682380cd27ba0e6403e24ef38b
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-12-18 13:14:08 +0300

    Add project files.

---
+++ Order.API/Models/Contexts/OrderDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.API.Models.Entites;
+
+namespace Order.API.Models.Contexts
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Models.Entites.Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+    }
+}
---
+++ Stock.Service/Consumers/OrderCreatedEventConsumer.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Stock.Service.Consumers
+{
+    public class OrderCreatedEventConsumer : IConsumer<OrderCreatedEventConsumer>
+    {
+        public async Task Consume(ConsumeContext<OrderCreatedEventConsumer> context)
+        {
+            // Sade bir şekilde inbox patterne odaklanacağımız için stock işlemlerini mış gibi yapıyoruz.
+            await Console.Out.WriteLineAsync(JsonSerializer.Serialize(context.Message));
+        }
+    }
+}
